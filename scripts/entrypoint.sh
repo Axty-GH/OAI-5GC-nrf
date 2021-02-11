e34387d5946625b3a3db28907a057c332b5ce707
arora <sagar.arora@eurecom.fr>
2021-02-11 20:23:23 +0100

    update ubuntu and rhel dockerfie

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,9 +1,3 @@
-#---------------------------------------------------------------------
-#
-# Docker entrypoint file for setting up environment variables and generating conf file
-#   Valid for Ubuntu-18.04 (bionic)
-#
-#---------------------------------------------------------------------
 #!/bin/bash
 
 set -euo pipefail
@@ -12,13 +6,13 @@ CONFIG_DIR="/openair-nrf/etc"
 for c in ${CONFIG_DIR}/*.conf; do
     # grep variable names (format: ${VAR}) from template to be rendered
     VARS=$(grep -oP '@[a-zA-Z0-9_]+@' ${c} | sort | uniq | xargs)
-
+    echo "Now setting these variables '${VARS}'"
     # create sed expressions for substituting each occurrence of ${VAR}
     # with the value of the environment variable "VAR"
     EXPRESSIONS=""
     for v in ${VARS}; do
 	NEW_VAR=`echo $v | sed -e "s#@##g"`
-        if [[ "${!NEW_VAR}x" == "x" ]]; then
+        if [[ -z ${!NEW_VAR+x} ]]; then
             echo "Error: Environment variable '${NEW_VAR}' is not set." \
                 "Config file '$(basename $c)' requires all of $VARS."
             exit 1
@@ -30,6 +24,5 @@ for c in ${CONFIG_DIR}/*.conf; do
     # render template and inline replace config file
     sed -i "${EXPRESSIONS}" ${c}
 done
-
+echo "Done setting the configuration"
 exec "$@"
-
