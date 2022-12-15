0f40a351c85740c126e2beca7f32c178c0425a92
Raphael Defosseux <raphael.defosseux@openairinterface.org>
2022-12-15 17:09:49 +0100

    fix(exit): catching SIGTERM
    Signed-off-by: Raphael Defosseux <raphael.defosseux@openairinterface.org>

--- src/oai-nrf/main.cpp
+++ src/oai-nrf/main.cpp
@@ -80,11 +80,8 @@ int main(int argc, char** argv) {
   Logger::init("nrf", Options::getlogStdout(), Options::getlogRotFilelog());
   Logger::nrf_app().startup("Options parsed");
 
-  struct sigaction sigIntHandler;
-  sigIntHandler.sa_handler = my_app_signal_handler;
-  sigemptyset(&sigIntHandler.sa_mask);
-  sigIntHandler.sa_flags = 0;
-  sigaction(SIGINT, &sigIntHandler, NULL);
+  std::signal(SIGTERM, my_app_signal_handler);
+  std::signal(SIGINT, my_app_signal_handler);
 
   // Config
   nrf_cfg.load(Options::getlibconfigConfig());
